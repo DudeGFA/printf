234d37809d7b508d8b0d02df967c726e4887050f
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-19 20:10:56 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,15 @@
 #ifndef _main_h
 #define _main_h
 #include <stdarg.h>
+/**
+ * struct prints_all - prints_all
+ * @sign: represents data type
+ * @print: a pointer function
+ */
+typedef struct prints_all
+{
+	char *sign;
+	void (*print)(va_list ap);
+} printer;
 int _printf(const char *format, ...);
 #endif /* main.h */
