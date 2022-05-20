d7899a4fa0289c0b69c4d9b320f2dbdb2399147d
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-20 15:14:06 +0100

    Create print_string.c

---
+++ print_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_string - prints a string
+ * 
+ * @ap: variable list 
+ * @format: parameter
+ * Return: no of printed letters
+ */
+int print_string(va_list ap, const char *format __attribute__((unused)), int i __attribute__((unused)))
+{
+    char *p;
+    int k;
+
+    p = va_arg(ap, char*);
+    for (k = 0; p[k]; k++)
+        _putchar(p[k]);
+    return (k - 1);
+}
