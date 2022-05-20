72f6b81d7100f2764ef1c9456d137f7b1f56b7ad
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-20 14:12:15 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdio.h>
 /**
  * _print - print all parameters
  * @format: list of types of arguments passed to the function
@@ -8,26 +9,27 @@
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int i = 0, j;
+	int i, j;
 	printer ss[] = {
-		{"c", print_char}, {"i", print_int},
+		{"c", print_char}, /*{"i", print_int},
 		{"d", print_decint},{"s", print_string},
 		{"b", print_in_bin},{"u", print_unint},
 		{"o", print_octint}, {"x", print_hex},
 		{"X", print_uphex}, {"S", print_string},
 		{"p", print_addr}, {"R", print_rot13},
-		{"r", print_rev}, {NULL, NULL},
+		{"r", print_rev},*/
+		{NULL, NULL},
 	};
 
 	va_start(ap, format);
-	for (i == 0; format[i] && format; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == "%")
+		if (format[i] == '%')
 		{
 				j = 0;
-				while (j < 4)
+				while (j < 1)
 				{
-					if (*(format + i) == *(ss[j].sign))
+					if (*(format + i + 1) == *(ss[j].sign))
 					{
 						ss[j].print(ap, format, i);
 					}
