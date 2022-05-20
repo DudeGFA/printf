c254278208a546d1cf0d44524e471f59200d4f83
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-20 14:11:16 +0100

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,9 +1,5 @@
 #include "main.h"
 #include <unistd.h>
-/**
- * _putchar - prints a char
- * return: prints and return the no of printed char
- */
 int _putchar(char c)
 {
     return(write(1, &c, 1));
