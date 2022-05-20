fc7aaaf9766bf475abf7de8a7bea4c99152671f8
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-20 19:26:10 +0100

    Update print_char.c

--- print_char.c
+++ print_char.c
@@ -7,11 +7,11 @@
  * @i: parameter
  * Return: int
  */
-int print_char(va_list ap, const char *f __attribute__((unused)), int i)
+int print_char(va_list ap, const char *f __attribute__((unused)), int i __attribute__((unused)))
 {
 	char c;
 
 	c = va_arg(ap, int);
 	_putchar(c);
-	return (i);
+	return (1);
 }
