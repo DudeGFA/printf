dd3112b44fa149f762a75fa9e12dc2f81339f30b
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-20 19:47:31 +0100

    Update print_perc.c

--- print_perc.c
+++ print_perc.c
@@ -7,8 +7,8 @@
  * @i: parameter
  * Return: int
  */
-int print_perc(va_list ap __attribute__((unused)), const char *f __attribute__((unused)), int i __attribute__((unused)))
+int print_perc(va_list ap __attribute__((unused)), const char *f __attribute__((unused)), int i)
 {
 	_putchar('%');
-	return (1);
+	return ((i * 0) + 1);
 }
