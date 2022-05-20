23ef0ea30145785f38bcf0672363d3f66062bb47
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-20 22:06:49 +0100

    Update print_char.c

--- print_char.c
+++ print_char.c
@@ -1,17 +1,15 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdarg.h>
 /**
- * print_char - prints a char
- * @ap: va_list
- * @f: parameter
- * @i: parameter
+ * print_char - prints a char 
+ * ap: va_list 
  * Return: int
  */
-int print_char(va_list ap, const char *f __attribute__((unused)), int i)
+int print_char(va_list ap, const char *format __attribute__((unused)), int i __attribute__((unused)))
 {
 	char c;
-
 	c = va_arg(ap, int);
 	_putchar(c);
-	return ((i * 0) + 1);
+	return (1);
 }
