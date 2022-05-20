dd9931ae51e046cc0313bbf36b04c326624ab8f9
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-20 15:15:09 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,15 +4,15 @@
 /**
  * _print - print all parameters
  * @format: list of types of arguments passed to the function
- * Return: void
+ * Return: int
  */
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int i, j;
+	int i, j, n = 0, m = 0;
 	printer ss[] = {
-		{"c", print_char}, /*{"i", print_int},
-		{"d", print_decint},{"s", print_string},
+		{"s", print_string},{"c", print_char},
+		/*{"d", print_decint},{"i", int},
 		{"b", print_in_bin},{"u", print_unint},
 		{"o", print_octint}, {"x", print_hex},
 		{"X", print_uphex}, {"S", print_string},
@@ -22,24 +22,25 @@ int _printf(const char *format, ...)
 	};
 
 	va_start(ap, format);
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format[i] && format; i++)
 	{
 		if (format[i] == '%')
 		{
-				j = 0;
-				while (j < 1)
+			m++;
+			j = 0;
+			while (j < 2)
+			{
+				if (*(format + i + 1) == *(ss[j].sign))
 				{
-					if (*(format + i + 1) == *(ss[j].sign))
-					{
-						ss[j].print(ap, format, i);
-					}
-					j++;
+					n += ss[j].print(ap, format, i);
 				}
-				i++;
+				j++;
+			}
+			i++;
 		}
 		else
 			_putchar(format[i]);
 	}
 	va_end(ap);
-    return (0);
+    return ((n - (2 * m)) + (i - 1));
 }
