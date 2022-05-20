7336332cce7cd20e31ec79a8f48acf56ff8d1baf
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-20 19:20:08 +0100

    Update print_perc.c

--- print_perc.c
+++ print_perc.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdarg.h>
 /**
- * print_perc - prints % 
+ * print_perc - prints %
  * @ap: va_list
  * @format: parameter
  * @i: parameter
- * Return: int 
+ * Return: int
  */
-int print_perc(va_list ap __attribute__((unused)),const char *format __attribute__((unused)), int i __attribute__((unused)))
+int print_perc(va_list ap, const char *f, int i)
 {
 	_putchar('%');
 	return (1);
