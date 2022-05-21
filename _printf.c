19fa832b69137ddc8f242184653c23b50e37ffe5
Xavier Kanu <88287669+xaviwho@users.noreply.github.com>
2022-05-21 13:18:09 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -10,20 +10,21 @@ int _printf(const char *format, ...)
 	va_list ap;
 	int i, j, m = 0, n = 0, ck = 0;
 	printer ss[] = {
-		{"s", print_string},{"c", print_char},
-/*{"d", print_decint} , {"i", int},
-{"b", print_in_bin},{"u", print_unint},
-{"o", print_octint}, {"x", print_hex},
-{"X", print_uphex}, {"S", print_string},
-{"p", print_addr}, {"R", print_rot13},
-{"r", print_rev},
+		{"s", print_string} , {"c", print_char},
+/**
+*{"d", print_decint} , {"i", int},
+*{"b", print_in_bin},{"u", print_unint},
+*{"o", print_octint}, {"x", print_hex},
+*{"X", print_uphex}, {"S", print_string},
+*{"p", print_addr}, {"R", print_rot13},
+*{"r", print_rev},
 */
 		{"%", print_perc}
 	};
 
 	if (format == NULL)
 	{
-		return (-1);
+		return(-1);
 	}
 	va_start(ap, format);
 	for (i = 0; format[i] && format; i++)
@@ -47,7 +48,7 @@ int _printf(const char *format, ...)
 		}
 		else
 			_putchar(format[i]);
-	}
+}
 	va_end (ap);
     return ((i - (n * 2)) + m - ck);
 }
