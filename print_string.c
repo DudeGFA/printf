c2cf421e2725bf4595834b572724486035d2db96
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-21 11:29:31 +0100

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -12,8 +12,16 @@ int print_string(va_list ap, const char *format __attribute__((unused)), int i _
 {
     char *p;
     int k;
+    char *f;
 
     p = va_arg(ap, char*);
+    if (p == (char *)0)
+    {
+        f = "(null)";
+        for(k = 0; f[k]; k++)
+            _putchar(f[k]);
+        return(6);
+    }
     for (k = 0; *(p + k); k++)
         _putchar(*(p + k));
     return (k);
