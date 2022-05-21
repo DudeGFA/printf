a70561ff6a1965a3aee81a0a40fd76dae738d3cd
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-21 22:55:42 +0100

    Update print_in_bin.c

--- print_in_bin.c
+++ print_in_bin.c
@@ -4,7 +4,7 @@
 #include <limits.h>
 int pr_uint2(int n)
 {
-    unsigned int k = 1;
+    int k = 1;
 
     if (n / 2 != 0)
         k += pr_uint2(n / 2);
@@ -13,8 +13,10 @@ int pr_uint2(int n)
 }
 int print_in_bin(va_list ap, const char *format __attribute__((unused)), int i __attribute__((unused)))
 {
-    unsigned int p;
+    int p;
 
     p = va_arg(ap, int);
+    if (p < 0)
+        return (-1);
     return (pr_uint2(p));
 }
