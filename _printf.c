ec8a0e5c9400b8008f232d29024c42f8986fca3c
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-21 13:57:48 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,27 +1,34 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
-/*_print - print all parameters
- *@format: list of types of arguments passed to the function
- *Return: int
+/**
+ * _print - print all parameters
+ * @format: list of types of arguments passed to the function
+ * Return: int
  */
 int _printf(const char *format, ...)
 {
 	va_list ap;
 	int i, j, m = 0, n = 0, ck = 0;
 	printer ss[] = {
-		{"s", print_string} , {"c", print_char},
+		{"s", print_string},{"c", print_char},
+		{"d", print_int},{"i", print_int},
+		/*{"b", print_in_bin},{"u", print_unint},
+		{"o", print_octint}, {"x", print_hex},
+		{"X", print_uphex}, {"S", print_string},
+		{"p", print_addr}, {"R", print_rot13},
+		{"r", print_rev},*/
 		{"%", print_perc}
 	};
 
 	if (format == NULL)
-		return(-1);
-	va_start(ap, format);
+		return (-1);
+	va_start (ap, format);
 	for (i = 0; format[i] && format; i++)
 	{
 		if (format[i] == '%')
 		{
-			for (j = 0; j < 3; j++)
+			for (j = 0; j < 5; j++)
 			{
 				if (*(format + i + 1) == *(ss[j].sign))
 				{
@@ -30,7 +37,7 @@ int _printf(const char *format, ...)
 					i++;
 					break;
 				}
-				if (j == 2)
+				if (j == 4)
 					ck++;
 			}
 		}
@@ -38,5 +45,5 @@ int _printf(const char *format, ...)
 			_putchar(format[i]);
 	}
 	va_end (ap);
-	return ((i - (n * 2)) + m - ck);
+    return ((i - (n * 2)) + m - ck);
 }
