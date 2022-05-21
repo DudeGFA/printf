9152fbcfb75618cb3ace0af9656748fb2943690c
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-21 19:04:51 +0100

    Update main.h

--- main.h
+++ main.h
@@ -18,28 +18,5 @@ int _printf(const char *format, ...);
 int print_char(va_list ap, const char *, int);
 int print_string(va_list ap, const char *format, int i);
 int print_perc(va_list ap, const char *format, int i);
-#endif /* main.h */
-=======
-#ifndef _main_h
-#define _main_h
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
-/**
- * struct prints_all - prints_all
- * @sign: represents data type
- * @print: a pointer function
- */
-typedef struct prints_all
-{
-	char *sign;
-	int (*print)(va_list ap, const char *, int);
-} printer;
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_char(va_list ap, const char *, int);
-int print_string(va_list ap, const char *format, int i);
-int print_perc(va_list ap, const char *format, int i);
 int print_int(va_list ap, const char *format, int i);
-
 #endif /* main.h */
