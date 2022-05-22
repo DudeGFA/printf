e0bad5732b8a57d305f261b0c092465b8dada743
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-22 16:45:09 +0100

    Update print_in_bin.c

--- print_in_bin.c
+++ print_in_bin.c
@@ -2,19 +2,19 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <limits.h>
-int pr_uint2(int n)
+int pr_bint(unsigned int n)
 {
-    int k = 1;
+    unsigned int k = 1;
 
-    if (n / 2 != 0)
-        k += pr_uint2(n / 2);
-    _putchar((n % 2) + '0');
+    if (n / 8 != 0)
+        k += pr_bint(n / 8);
+    _putchar((n % 8) + '0');
     return (k);
 }
 int print_in_bin(va_list ap, const char *format __attribute__((unused)), int i __attribute__((unused)))
 {
     unsigned int p;
 
-    p = va_arg(ap, int);
-    return (pr_uint2(p));
+    p = va_arg(ap, unsigned int);  
+    return (pr_bint(p));
 }
