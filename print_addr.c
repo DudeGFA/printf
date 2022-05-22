f5ea39caa517bdf7ada8102fffe5a914a3282e8d
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-23 00:22:45 +0100

    Update print_addr.c

--- print_addr.c
+++ print_addr.c
@@ -4,7 +4,6 @@ int print_addr(va_list ap, const char *format __attribute__((unused)), int i __a
     unsigned int p;
 
     p = va_arg(ap, int);
-    printf("%d\n", p);
     _putchar('0');
     _putchar('x');
     return ((pr_uuxint(p)) + 2);
