53cb67e5fba879ea285b25159081fe1b316b1d84
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-25 22:09:49 +0100

    Update print_rev.c

--- print_rev.c
+++ print_rev.c
@@ -7,13 +7,18 @@
  */
 int print_rev(va_list ap __attribute__((unused)),const char *format __attribute__((unused)), int i __attribute__((unused)))
 {
-	char *str;
-	int y, b;
-
-	str = va_arg(ap, char *);
-	for (y = 0; str[y]; y++)
-		b = y;
-	for (y--; y >= 0; y--)
-		_putchar(str[y]);
-	return (b + 1);
+    char *str;
+    int y, b;
+    str = va_arg(ap, char *);
+    if (str == NULL)
+        return (-1);
+    if (str[0] != '\0')
+    {
+        for (y = 0; str[y]; y++)
+        b = y;
+        for (y--; y >= 0; y--)
+            _putchar(str[y]);
+        return (b + 1);
+    }
+    return (0);
 }
