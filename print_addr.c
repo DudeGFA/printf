b32c3d2166fe360867180cfb510a723b73097a74
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-26 01:08:41 +0100

    Update print_addr.c

--- print_addr.c
+++ print_addr.c
@@ -1,10 +1,11 @@
 #include "main.h"
 int print_addr(va_list ap, const char *format __attribute__((unused)), int i __attribute__((unused)))
 {
-    unsigned int p;
-
-    p = va_arg(ap, int);
-    _putchar('0');
-    _putchar('x');
-    return ((pr_uuxint(p)) + 2);
+    char *p;
+    p = va_arg(ap, char *);
+    printf("%p", p);
+   /* for (l = 0; p[l]; l++)
+        _putchar[p[l]];*/
+    write(1, p, 16);
+    return (16);
 }
